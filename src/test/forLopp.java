2e84ce47f0f106e4473c86af002c29bcaef950ff
meg12345645 <rajmegha1793@gmail.com>
2023-03-19 12:10:54 +0530

    third commit

---
+++ src/test/forLopp.java
@@ -0,0 +1,18 @@
+package test;
+
+public class forLopp {
+	
+	public static void main(String[]args) {
+	
+	
+	String[] name = {"megha" , "raj" , "test"};
+	
+	for (String s : name) {
+		
+		System.out.println(s);
+	}
+
+
+}
+
+}
